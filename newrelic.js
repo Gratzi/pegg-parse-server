cb341d72f014d4c8cdf6abcaba82bd2d89fa5ea1
Auggernaut <augustin@datacosmos.com>
2016-12-07 10:01:59 -0800

    add newrelic

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Pegg-staging', 'Pegg'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '1101dfa73a1f210ed1cc8aeca70c4b461eb7f11f',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
