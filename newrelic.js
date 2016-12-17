ab6ce649adf0eea8bafb4b4401c0a8af685dbc6e
Jack Senechal <jacksenechal@gmail.com>
2016-12-16 18:13:00 -0800

    Revert node-pushnotification changes
    This reverts commit bc3a763442614d1ea28fa4491bb54ad697e33b79.

--- newrelic.js
+++ newrelic.js
@@ -10,7 +10,7 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: ['Pegg-' + process.env.PEGG_ENV],
+  app_name: ['Pegg-' + process.env.NODE_ENV],
   /**
    * Your New Relic license key.
    */
